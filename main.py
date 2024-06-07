7788edfb1870365c156f55b78c9289b29c1e6571
mr.sahu <70091452+mr-sahu2002@users.noreply.github.com>
2024-06-08 05:29:04 +0530

    last update

--- main.py
+++ main.py
@@ -2,10 +2,7 @@ import google.generativeai as genai
 from pathlib import Path
 import streamlit as st
 from streamlit_option_menu import option_menu
-import requests
-import io
 from PIL import Image
-
 
 #api initialize
 genai.configure(api_key="AIzaSyCh44U7jDRCK5TOdVtqIVY8tsRjnShzYYk")
@@ -40,10 +37,10 @@ model = genai.GenerativeModel(model_name = "gemini-1.5-pro",
 generation_config = generation_config,
 safety_settings = safety_settings)
 
-#fronted part
+## fronted part
 col1, col2 = st.columns(2)
 with st.sidebar:
-    select_op = option_menu('Home', ["OCR",'ChatBot',"CodeToFlow",'About Us'],icons = ['house','chat','upload','person'] ,default_index=0)
+    select_op = option_menu('Home', ["OCR",'ChatBot','About Us'],icons = ['house','chat','person'] ,default_index=0)
 if select_op == "OCR":
 
     imageQuestion = st.text_input("Bol bhai")
@@ -75,6 +72,7 @@ if select_op == "OCR":
 
         st.write(response.text)
 
+#################### chatBot #####################
 #
 if "conversation_started" not in st.session_state:
     st.session_state.conversation_started = False
@@ -83,7 +81,6 @@ if "chat_history" not in st.session_state:
 if "user_persona" not in st.session_state:
     st.session_state.user_persona = None
 #
-
 
 #code for personas
 
@@ -99,9 +96,6 @@ def generate_persona_prompt(persona, user_input):
 
 #
 
-
-
-
 #newest shit 11:56
 
 if select_op == "ChatBot":
@@ -111,7 +105,6 @@ if select_op == "ChatBot":
         persona = st.selectbox("Who are you?", persona_options)
         # Language_options = ['English','Hindi','Japanese']
         # lan = st.selectbox("Which Language do you prefer",Language_options)
-        
         user_input = st.text_input("You:")
         
         if st.button("Talk"):
@@ -120,8 +113,8 @@ if select_op == "ChatBot":
             st.session_state.chat_model = genai.GenerativeModel(model_name="gemini-1.0-pro")
             prompt = generate_persona_prompt(persona, user_input)
             ai = st.session_state.chat_model.generate_content(prompt)
-            st.session_state.chat_history.append(("You", user_input))
-            st.session_state.chat_history.append(("Bot", ai.text))
+            st.session_state.chat_history.append(('🧑', user_input))
+            st.session_state.chat_history.append(('🤖', ai.text))
             st.experimental_rerun()
     else:
         for speaker, message in st.session_state.chat_history:
@@ -135,49 +128,110 @@ if select_op == "ChatBot":
                 if user_input:
                     prompt = generate_persona_prompt(st.session_state.user_persona, user_input)
                     ai = st.session_state.chat_model.generate_content(prompt)
-                    st.session_state.chat_history.append(("You", user_input))
-                    st.session_state.chat_history.append(("Bot", ai.text))
+                    st.session_state.chat_history.append(('🧑', user_input))
+                    st.session_state.chat_history.append(('🤖', ai.text))
                     st.experimental_rerun()
         
         with col2:
-            if st.button("End"):
+            if st.button("Clear"):
                 st.session_state.conversation_started = False
                 st.session_state.chat_history = []
                 st.session_state.user_persona = None
                 st.experimental_rerun()
 
-#
+######### About us ##########
 
-#CodeToFlow part
-API_URL = "https://api-inference.huggingface.co/models/Corcelio/mobius"
-headers = {"Authorization": "Bearer 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"}
+if select_op == "About Us":
+    
+    #github url of everyone
+    paul_git_url = 'https://github.com/AbhishekPaul08'
+    kushal_git_url = 'https://github.com/Kushal1221'
+    sahu_git_url = 'https://github.com/mr-sahu2002'
+    vivek_git_url = 'https://github.com/Vivek-Hello'
 
-if(select_op=="CodeToFlow"):
-    Code = st.text_input("Enter your code:")
-    prompt= f'Write a prompt for generate flow chart for given code {Code}'
+    #linkedin url of everyone
+    paul_link_url = 'https://www.linkedin.com/in/abhishek-paul-p-955627282/'
+    kushal_link_url = 'https://www.linkedin.com/in/kushal-k-123908248/'
+    sahu_link_url = 'www.linkedin.com/in/durgamadhab-sahu-8b6b21215'
+    vivek_link_url = 'www.linkedin.com/in/vivek-r-23b716307'
 
-    Image_Mod=genai.GenerativeModel(model_name = "gemini-1.0-pro")
-    response=Image_Mod.generate_content(prompt)
+    github_logo = "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+    linkedin_logo = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/640px-LinkedIn_logo_initials.png'
 
 
-    if(st.button("submit")):
-        def query(payload):
-            response = requests.post(API_URL, headers=headers, json=payload)
-            return response.content
-        image_bytes = query({
-        "inputs": response.text,
-        })
-        image = Image.open(io.BytesIO(image_bytes))
-        st.image(image)
+    #Abhishek profile
+    colImage,colInfo=st.columns(2)
+    with colImage:
+        st.image('images/abhishek.jpg')
+    with colInfo:
+        st.header("Hi I'm Abhishek Paul")
+        col1, col2 = st.columns(2)
+        
+        with col1:
+            st.write("My Github profile")
+            image_html = f'<a href="{paul_git_url}"><img src="{github_logo}" alt="Icon" width="50" height="50">'
 
-# def query(payload):
-# 	response = requests.post(API_URL, headers=headers, json=payload)
-# 	return response.content
-# image_bytes = query({
-# 	"inputs": "Astronaut riding a horse",
-# })
-# # You can access the image with PIL.Image for example
-# import io
-# from PIL import Image
-# image = Image.open(io.BytesIO(image_bytes))
+            st.markdown(image_html, unsafe_allow_html=True)
 
+        with col2:    
+            st.write("My linkedin profile")
+            link_html = f'<a href="{paul_link_url}"><img src="{linkedin_logo}" alt="Icon" width="50" height="50">'
+
+            st.markdown(link_html, unsafe_allow_html=True)
+   
+    
+    #Kushal profile
+    colImage,colInfo=st.columns(2)
+    with colImage:
+        st.image('images/kushal.jpg')
+    with colInfo: 
+        st.header("Hi I'm Kushal K")
+        col1, col2 = st.columns(2)
+       
+        with col1:
+            st.write("My Github profile")
+            image_html = f'<a href="{kushal_git_url}"><img src="{github_logo}" alt="Icon" width="50" height="50">'
+            st.markdown(image_html, unsafe_allow_html=True)
+
+        with col2:
+            st.write("My linkedin profile")
+            link_html = f'<a href="{kushal_link_url}"><img src="{linkedin_logo}" alt="Icon" width="50" height="50">'
+
+            st.markdown(link_html, unsafe_allow_html=True)
+  
+    #Sahu profile
+    colImage,colInfo=st.columns(2)
+    with colImage:
+        st.image('images/sahu.jpg')
+    with colInfo:
+        st.header("Hi I'm Sahu Durgamadhab")
+        col1, col2 = st.columns(2)
+        
+        with col1:
+            st.write("My Github profile")
+            image_html = f'<a href="{sahu_git_url}"><img src="{github_logo}" alt="Icon" width="50" height="50">'
+            st.markdown(image_html, unsafe_allow_html=True)
+        with col2: 
+            st.write("My linkedin profile")
+            link_html = f'<a href="{sahu_link_url}"><img src="{linkedin_logo}" alt="Icon" width="50" height="50">'
+
+            st.markdown(link_html, unsafe_allow_html=True)
+        
+    #Vivek profile
+    colImage,colInfo=st.columns(2)
+    with colImage:
+        st.image('images/vivek.jpg')
+    with colInfo: 
+        st.header("Hi I'm Vivek R")
+        col1, col2 = st.columns(2)  
+         
+        with col1:
+            st.write("My Github profile")
+            image_html = f'<a href="{vivek_git_url}"><img src="{github_logo}" alt="Icon" width="50" height="50">'
+            st.markdown(image_html, unsafe_allow_html=True)
+
+        with col2: 
+            st.write("My linkedin profile")
+            link_html = f'<a href="{vivek_link_url}"><img src="{linkedin_logo}" alt="Icon" width="50" height="50">'
+
+            st.markdown(link_html, unsafe_allow_html=True)
